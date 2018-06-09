cd0b61f20e1d65d7aecb73b06cb6f8e7400af75e
hzphzp <huangzp1104@gmail.com>
2018-06-09 09:48:39 +0800

    add the web client test

--- src/Servlet.java
+++ src/Servlet.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Enumeration;
 
 @WebServlet(name = "Servlet")
 public class Servlet extends HttpServlet {
@@ -18,6 +20,8 @@ public class Servlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setIntHeader("Refresh", 5);
+        Date date = new Date();
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
         massage = request.getParameter("massage");
@@ -27,6 +31,15 @@ public class Servlet extends HttpServlet {
         writer.println(
                 "<h1>"+massage+order+id+action+"</h1>"
         );
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            String paraName = (String) headerNames.nextElement();
+            writer.println("<tr><td>" + paraName + "</td>\n");
+            String paraValue = request.getHeader(paraName);
+            writer.println("<td>" + paraValue + "</td></tr>\n");
+            writer.println("<h1>" + date.toString() + "</h1>");
+        }
+        response.sendError(407, "Nedd Authrorize");
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
