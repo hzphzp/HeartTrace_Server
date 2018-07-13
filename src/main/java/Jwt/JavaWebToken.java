5b535be642cd4e895ad9afc4e309412012077c14
hzphzp <huangzp1104@gmail.com>
2018-07-14 00:13:23 +0800

    finish token

--- src/main/java/Jwt/JavaWebToken.java
+++ src/main/java/Jwt/JavaWebToken.java
@@ -1,4 +1,30 @@
 package Jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 public class JavaWebToken {
+    public String createToken(){
+        String token = null;
+        try{
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            token = JWT.create().withIssuer("huangzp").sign(algorithm);
+        }catch (JWTCreationException exception){
+            //Invalid Signing configuration / Couldn't convert Claims.
+            exception.printStackTrace();
+        }
+        return token;
+    }
+    public void verifyToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer("huangzp").build();
+            DecodedJWT jwt = verifier.verify(token);
+        }catch (JWTCreationException exception){
+            exception.printStackTrace();
+        }
+    }
 }
--- src/main/java/Jwt/Payload.java
+++ src/main/java/Jwt/Payload.java
@@ -1,4 +1,11 @@
 package Jwt;
 
 public class Payload {
+    public String iss;
+    public String sub;
+    public String aud;
+    public String exp;
+    public String nbf;
+    public String iat;
+    public String jti;
 }
