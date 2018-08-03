7285f17dac5fe6335077eaba39ac722cb1685ae0
hzphzp <huangzp1104@gmail.com>
2018-08-03 21:41:56 +0800

    nothing

--- src/main/java/Servlet/UploadFile.java
+++ src/main/java/Servlet/UploadFile.java
@@ -5,14 +5,19 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import sun.misc.BASE64Decoder;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -39,88 +44,25 @@ public class UploadFile extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        /**
-         * 上传数据及保存文件
-         */
-        // 检测是否为多媒体上传
-        //DownloadFile.isMultipart(request, response);
         String username = request.getParameter("username");
-        String token = request.getParameter("token");
         String modelnum = request.getParameter("modelnum");
-        Collection<Part> parts =  request.getParts();
-
-        Long anchor = Long.parseLong(request.getParameter("anchor"));
+        String token = request.getParameter("token");
         JavaWebToken.verifyToken(token, username, modelnum);
-
-
-        // 配置上传参数
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        // 设置内存临界值 - 超过后将产生临时文件并存储于临时目录中
-        factory.setSizeThreshold(MEMORY_THRESHOLD);
-        // 设置临时存储目录
-        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
-
-        ServletFileUpload upload = new ServletFileUpload(factory);
-
-        // 设置最大文件上传值
-        upload.setFileSizeMax(MAX_FILE_SIZE);
-
-        // 设置最大请求值 (包含文件和表单数据)
-        upload.setSizeMax(MAX_REQUEST_SIZE);
-
-        // 中文处理
-        upload.setHeaderEncoding("UTF-8");
-
-        // 构造临时路径来存储上传的文件
-        // 这个路径相对当前应用的目录
+        String filename = request.getParameter("filename");
+        String filetype = request.getParameter("filetype");
+        String content = request.getParameter("content");
         String uploadPath = request.getServletContext().getRealPath("./") + File.separator + username;
-
-
-        // 如果目录不存在则创建
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) {
-            uploadDir.mkdir();
+        File dir = new File(uploadPath);
+        if(!dir.exists()){
+            dir.mkdir();
         }
-
-        try {
-            // 解析请求的内容提取文件数据
-            @SuppressWarnings("unchecked")
-            List<FileItem> formItems = upload.parseRequest(request);
-
-            if (formItems != null && formItems.size() > 0) {
-                // 迭代表单数据
-                for (FileItem item : formItems) {
-                    // 处理不在表单中的字段
-                    if (!item.isFormField()) {
-                        String fileName = new File(item.getName()).getName();
-                        String filePath = uploadPath + File.separator + fileName;
-                        File storeFile = new File(filePath);
-                        // 在控制台输出文件的上传路径
-                        System.out.println(filePath);
-                        // 保存文件到硬盘
-                        item.write(storeFile);
-                        request.setAttribute("message",
-                                "文件上传成功!");
-                    }
-                }
-            }
-        } catch (Exception ex) {
-            request.setAttribute("message",
-                    "错误信息: " + ex.getMessage());
-        }
-        File[] arrayFiles = uploadDir.listFiles();
-        for(int i = 0; i < arrayFiles.length; i++){
-            if(arrayFiles[i].isDirectory()){
-                continue;
-            }
-            else if (arrayFiles[i].isFile()){
-                if(arrayFiles[i].lastModified() > anchor){
-                    //需要返回同步
-                    response.getWriter().println(arrayFiles[i].getName());
-                    //将文件名返回
-                }
-            }
-        }
+        log("huangzp");
+        String filePath = dir + File.separator + filename;
+        File fileStore = new File(filePath);
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] bytes = decoder.decodeBuffer(content);
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        fileStore.createNewFile();
+        BufferedImage bfi = 
     }
 }
